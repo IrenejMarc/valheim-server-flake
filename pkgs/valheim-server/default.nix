8815932d79227cb327e8a4b34663815d254d7024
Irenej Marc <irenej.marc@gmail.com>
2026-09-13 10:10:19 +0200

    Update Valheim server yet again

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -10,8 +10,8 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "6242541463332331733";
-    hash = "sha256-XuFpPUhdUPY1wq7Y3EEwhUtv4L+hENT0aV/Zd8BWg1M=";
+    manifestId = "9055200629726788899";
+    hash = "sha256-ZnMaPlLwXiKYoJmkIcMqtLr9Ce5CioIn4pZr00demoo=";
   };
 
   # Skip phases that don't apply to prebuilt binaries.
